a614a318b832f7981821af817af1d8b5e41a973f
AndhreSoriano <daitiut@gmail.com>
2026-05-05 09:15:34 -0500

    Update productos.js

--- frontend/src/api/productos.js
+++ frontend/src/api/productos.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/productos';
+const API_URL = import.meta.env.VITE_API_URL || '/api/productos';
 
 export const obtenerProductos = (params = {}) => 
   axios.get(API_URL, { params }).then(res => res.data);
